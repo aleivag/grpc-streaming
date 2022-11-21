2ab85c3c65a45f748d99f02b4774b534fbe9dc3b
Alvaro Leiva Geisse <aleivag@gmail.com>
2022-11-20 22:48:35 -0800

    fix small things

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,6 @@ set -ex
 
 VENV=./.venv
 PYTHON=$VENV/bin/python3
-ROOT_DIR=proto
 
-test -d $VENV || $(python3 -m venv $VENV && $PYTHON -m pip install requirements.txt )
+test -d $VENV || $(python3 -m venv $VENV && $PYTHON -m pip install -r requirements.txt )
 $PYTHON -m grpc_tools.protoc -I . --python_out=.  --grpc_python_out=. proto/*.proto
