0725ab228b81dd0cbc41611fb63f5219ef9bf955
Alvaro Leiva Geisse <aleivag@gmail.com>
2022-11-20 22:56:00 -0800

    add boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ set -ex
 VENV=./.venv
 PYTHON=$VENV/bin/python3
 
-test -d $VENV || $(python3 -m venv $VENV && $PYTHON -m pip install -r requirements.txt )
+test -d $VENV || (python3 -m venv $VENV && $PYTHON -m pip install -r requirements.txt )
 $PYTHON -m grpc_tools.protoc -I . --python_out=.  --grpc_python_out=. proto/*.proto
